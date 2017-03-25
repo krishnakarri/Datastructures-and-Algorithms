db6829e2fb236982d01d667835c161d44e1c1e8a
jkarri <krishna.karri789@gmail.com>
2017-03-25 11:22:09 -0700

    Created Trees project and added binary tree source

---
+++ Trees/src/binaryTrees/BinaryNode.java
@@ -0,0 +1,16 @@
+package binaryTrees;
+
+public class BinaryNode {
+
+	int data;
+	BinaryNode left;
+	BinaryNode right;
+	
+	
+	public BinaryNode(int data, BinaryNode left, BinaryNode right) {
+		this.data = data;
+		this.left = left;
+		this.right = right;
+	}
+
+}
